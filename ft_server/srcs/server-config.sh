88d1e1ccf4c6c517b1018ac7ca39f02449b0242b
brunovmc <bruno.vidigal.coriolano@gmail.com>
2020-05-27 01:43:40 +0200

    codealong260520

---
+++ ft_server/srcs/server-config.sh
@@ -0,0 +1,7 @@
+mkdir /var/www/localhost
+
+cp /root/nginx.conf /etc/nginx/sites-available/default
+
+# ln -s /etc/nginx/sites-available/localhost/ /etc/nginx/sites-enabled/
+
+cp /root/info.php /var/www/localhost/
