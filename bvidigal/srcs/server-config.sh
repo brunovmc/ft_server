c3dbe38cfd0322906d34a2f1180001ae92cf5702
brunovmc <bruno.vidigal.coriolano@gmail.com>
2020-05-27 00:32:28 +0200

    dockerfile start config

---
+++ bvidigal/srcs/server-config.sh
@@ -0,0 +1,11 @@
+apt-get install -y nginx \
+	php7.3 \
+	php-fpm \
+	php-mysql \
+	php-cli \
+	php-mbstring \
+	mariadb-server
+
+
+
+
---
+++ bvidigal/srcs/start.sh
@@ -0,0 +1,2 @@
+service nginx start && \
+	service mysql start
