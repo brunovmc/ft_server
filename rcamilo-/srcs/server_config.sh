d6ad55085d91c70318b35c6fc7df8922a246231e
rcamiloo <rodrigo.camilo@gmail.com>
2020-05-28 21:54:52 -0300

    ultimo dia

--- rcamilo-/srcs/server_config.sh
+++ rcamilo-/srcs/server_config.sh
@@ -2,11 +2,9 @@
 
 apt-get update
 apt-get -y  upgrade
-apt-get install -y mariadb-server 	\
-		   nginx		\
-		   php7.3 php-fpm php-mysql php-cli php-mbstring
 
-mkdir /var/www/localhost
 
-bash /root/services_config.sh
+
+
+
 
--- rcamilo-/srcs/services_config.sh
+++ rcamilo-/srcs/services_config.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+apt-get install -y --no-install-recommends apt-utils	\
+			mariadb-server 	\
+			nginx		\
+			php7.3 php7.3-fpm php7.3-mysql php7.3-cli php7.3-mbstring	\
+			wget
+
+
+mkdir /var/www/localhost
+
+cd /root
+
+# MyPhpAdmin instalação
+wget --no-check-certificate https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-english.tar.gz
+tar -xf phpMyAdmin-5.0.2-english.tar.gz && rm -rf phpMyAdmin-5.0.2-english.tar.gz
+mv phpMyAdmin-5.0.2-english /var/www/localhost/phpmyadmin
+cp -pr /root/config.inc.php /var/www/localhost/phpmyadmin/config.inc.php
 cp -pr /root/nginx.conf /etc/nginx/sites-available/default
+chown -R www-data:www-data /var/www/localhost/phpmyadmin
+
+# Informações de PHP
 cp /root/info.php /var/www/localhost
+
+
+## cria DB e acesso root
+echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
+mysqladmin --user=root password ""
+
+
+mv /root/info.php /var/www/localhost
 
 
